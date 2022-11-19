0a186f35452a5f7ede92720920edbe89fa76d662
Denis <icestrelok@gmail.com>
2022-11-19 21:54:04 +0300

    Check File

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from os import environ
+import os.path
 import dotenv
+import json
+import requests
+
 
 
 if __name__ == "__main__":
 	dotenv.load_dotenv('.env')
 	key = environ["vt_api"]
+	api_url = 'https://www.virustotal.com/vtapi/v2/file/scan'
+	filename = input("Enter a 'file path' :>")
+	params = dict(apikey=key)
+	if os.path.exists(filename):
+		with open(filename, 'rb') as file:
+			files = dict(file=(filename, file))
+			response = requests.post(api_url, files=files, params=params)
+		if response.status_code == 200:
+			result = response.json()
+			print(json.dumps(result, sort_keys=False, indent=4))
+	else:
+		print("File not found...")
